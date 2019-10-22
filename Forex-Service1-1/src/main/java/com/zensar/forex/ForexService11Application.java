d28d5f87e370ca11d8bafab3219b0fcdf46224f9
aditee-c <55917443+aditee-c@users.noreply.github.com>
2019-10-22 14:52:50 +0530

    Add files via upload

---
+++ Forex-Service1-1/src/main/java/com/zensar/forex/ForexService11Application.java
@@ -0,0 +1,13 @@
+package com.zensar.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexService11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexService11Application.class, args);
+	}
+
+}
---
+++ currency-conversion-service/src/main/java/com/zensar/currency/conversion/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,14 @@
+package com.zensar.currency.conversion;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name = "Forex-Service", url="localhost:8000")
+@FeignClient(name="Forex-Service")
+@RibbonClient(name="Forex-Service")//load balancer
+public interface CurrencyExchangeServiceProxy {
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retriveExchangeValue(@PathVariable("from") String from,@PathVariable("to") String to);
+}
